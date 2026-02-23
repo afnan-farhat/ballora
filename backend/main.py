6567f56f8eb77a95d66177ff10240120a7a6e942
afnan-farhat <4fnan.f@gmail.com>
2026-02-23 20:49:36 +0300

    return the allow_origins ti allow onluy origins

--- backend/main.py
+++ backend/main.py
@@ -63,7 +63,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=[r"https://.*\.vercel\.app"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
